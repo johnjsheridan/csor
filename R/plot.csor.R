38d9e22cc30b256200b4dfbe8119dabecd755b3f
johnjsheridan <jsheridan76@gmail.com>
2016-11-26 15:51:14 +0000

    Made changes to clean up the plots and add an option to choose between line plots per sector or a stacked bar chart.

--- R/plot.csor.R
+++ R/plot.csor.R
@@ -1,19 +1,36 @@
 library(ggplot2)
 
-plot.csor = function(obj, SecCode = 0, SeaAdj = FALSE)
+plot.csor = function(obj, SecCode = 0, SeaAdj = FALSE, type = "l")
 {
   data = obj$data
 
-  subData = subset(data, (SectorCode == SecCode) & (SeasonallyAdjusted == SeaAdj))
+  if(type == "l")
+  {
+    subData = subset(data, (SectorCode == SecCode) & (SeasonallyAdjusted == SeaAdj))
 
-  ggplot(subData, aes(x = numericQuarter, y = value)) +
-    geom_line() +
-    xlab('Time Period') +
-    ylab('GDP in Euro Millions')
+    graphtitle = subData$Statistic[1]
+    graphtitle = gsub("annually", "annually\n", graphtitle)
+    graphtitle = paste(graphtitle, "\n", subData$Sector[1], sep = "")
 
-  # Stacked bar chart
-  subDataStacked = subset(data, (SectorCode != 0) & (SeasonallyAdjusted == SeaAdj))
+    ggplot(subData, aes(x = numericQuarter, y = value)) +
+      theme_bw() +
+      ggtitle(graphtitle) +
+      geom_line() +
+      xlab('Time Period') +
+      ylab('GDP in Euro Millions')
+  }
+  else
+  {
+    subData = subset(data, (SectorCode != 0) & (SectorCode != 2) & (SeasonallyAdjusted == SeaAdj))
 
-  ggplot(subDataStacked, aes(x = numericQuarter, y = value ,fill = SectorCode)) +
-    geom_bar(stat='identity')
+    graphtitle = subData$Statistic[1]
+    graphtitle = gsub("annually", "annually\n", graphtitle)
+
+    ggplot(subData, aes(x = numericQuarter, y = value ,fill = Sector)) +
+      geom_bar(stat='identity') +
+      theme_bw() +
+      ggtitle(graphtitle) +
+      xlab('Time Period') +
+      ylab('GDP in Euro Millions')
+  }
 }
