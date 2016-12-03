5ba26718d2dbabcf8d10dfb6715f3c262b34c55c
johnjsheridan <jsheridan76@gmail.com>
2016-12-03 13:18:50 +0000

    Added the roxygen template.

--- R/plot.csor.R
+++ R/plot.csor.R
@@ -1,5 +1,21 @@
-library(ggplot2)
-
+#' Plot the GDP data in various ways
+#'
+#' @param obj An object of class \code{csor} from \code{\link{loadGDP}}
+#' @param SecCode The sector code for the sector to fit
+#' @param SeaAdj An indicator of whether we are fitting to the seasonally adjusted (TRUE) or the non-seasonally adjusted (FALSE) data
+#' @param type The type of plot to create. "l" for a straight line plot. "lf" for a points plot with a fitted smoothing spline line. "b" for a stacked bar chart of all sectors.
+#'
+#' @return Nothing generates a plot
+#' @export
+#' @import ggplot2
+#'
+#' @seealso \code{\link{loadGDP}}, \code{\link{plot.csor}}, \code{\link{sectorList}}, \code{\link{predict.csor}}, \code{\link{smooth.spline}}
+#' @examples
+#' #' data = loadGDP()
+#' plot(data)
+#' plot(data, SecCode = 4, SeaAdj = TRUE)
+#' plot(data, SecCode = 2, SeaAdj = FALSE, type = "lf")
+#' plot(data, SeaAdj = FALSE, type = "b")
 plot.csor = function(obj, SecCode = 0, SeaAdj = FALSE, type = "l")
 {
   # Extract the data from our object
